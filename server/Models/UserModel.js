4c92ca38dbec27dc887f06656c4a08cad2b53640
tanhoang0710 <tanhoang0710@gmail.com>
2022-11-30 16:32:16 +0700

    UI register

--- server/Models/UserModel.js
+++ server/Models/UserModel.js
@@ -10,6 +10,6 @@ const UserSchema = new mongoose.Schema({
 	role: String,
 });
 
-const User = mongoose.model('User', UserSchema);
+const UserModel = mongoose.model('User', UserSchema);
 
-module.exports = User;
+module.exports = UserModel;

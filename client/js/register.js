c9608a8260cc8b20a88ddfdcc627612cfbfe46ba
tanhoang0710 <tanhoang0710@gmail.com>
2022-12-01 10:57:16 +0700

    redirect login page

--- client/js/register.js
+++ client/js/register.js
@@ -1,19 +1,24 @@
-const handleRegister = () => {
+const handleRegister = async () => {
 	// 1. Lay value tren form
 	const username = document.getElementById('username').value;
 	const email = document.getElementById('email').value;
 	const password = document.getElementById('password').value;
 	// 2. Gui value tu client den server
-	axios
-		.post('http://localhost:5000/api/auth/register', {
-			username,
-			email,
-			password,
-		})
-		.then(function (response) {
-			console.log(response);
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+
+	try {
+		const res = await axios.post(
+			'http://localhost:5000/api/auth/register',
+			{
+				username,
+				email,
+				password,
+			}
+		);
+		console.log('🚀 ~ file: register.js:14 ~ response', response);
+		if (response.status === 201) {
+			window.location.href = '/client/login.html';
+		}
+	} catch (error) {
+		// log error
+	}
 };

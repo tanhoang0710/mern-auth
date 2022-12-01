a44a8afcee44434fb26108d6dd723dd533c076af
tanhoang0710 <tanhoang0710@gmail.com>
2022-12-01 09:45:23 +0700

    get form value

---
+++ client/js/register.js
@@ -0,0 +1,19 @@
+const handleRegister = () => {
+	// 1. Lay value tren form
+	const username = document.getElementById('username').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	// 2. Gui value tu client den server
+	axios
+		.post('http://localhost:5000/users', {
+			username,
+			email,
+			password,
+		})
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+};

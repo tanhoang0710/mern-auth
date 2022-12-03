18656b41edce34151884c70e28df8e59e58157f4
tanhoang0710 <tanhoang0710@gmail.com>
2022-12-03 10:27:21 +0700

    decode jwt at client

--- client/js/login.js
+++ client/js/login.js
@@ -10,6 +10,23 @@ const handleLogin = async () => {
 		console.log('🚀 ~ file: register.js:14 ~ res', res);
 		if (res.status === 200) {
 			// window.location.href = '/client/login.html';
+			const accessToken = res.data.data.accessToken;
+			// decode lay ra thong tin payload
+
+			const payloadDecoded = jwt_decode(accessToken);
+			console.log(
+				'🚀 ~ file: login.js:17 ~ handleLogin ~ payloadDecoded',
+				payloadDecoded
+			);
+
+			if (payloadDecoded.role === 'regular') {
+				window.location.href = '/client/home_page.html';
+			} else {
+				window.location.href = '/client/admin_page.html';
+			}
+
+			// save accesstoken
+			localStorage.setItem('accessToken', accessToken);
 		}
 	} catch (error) {
 		// log error

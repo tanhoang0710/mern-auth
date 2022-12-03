7aa5a06d695864709b99f0f24c3fc33deabd49dd
tanhoang0710 <tanhoang0710@gmail.com>
2022-12-03 11:54:04 +0700

    try catch api

--- client/js/admin-page.js
+++ client/js/admin-page.js
@@ -1,5 +1,20 @@
-axios.get('http://localhost:5000/auth/admin/user', {
-	headers: {
-		Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-	},
-});
+const getListUser = async () => {
+	try {
+		const configHeader = {
+			headers: {
+				Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+			},
+		};
+
+		const res = await axios.get(
+			'http://localhost:5000/auth/admin/user',
+			configHeader
+		);
+	} catch (error) {
+		// console.log(error);
+		// call refresh token
+		// token expired->redirect to admin
+	}
+};
+
+getListUser();

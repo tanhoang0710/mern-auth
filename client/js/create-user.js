93ddf1c78182e5b536621222a23b09964ac3374e
tanhoang0710 <tanhoang0710@gmail.com>
2022-12-06 16:15:53 +0700

    send api create user

---
+++ client/js/create-user.js
@@ -0,0 +1,23 @@
+const handleSubmitAddUser = async () => {
+	// 1. Lay value tren form
+	const username = document.getElementById('username').value;
+	const email = document.getElementById('email').value;
+	const password = document.getElementById('password').value;
+	const role = document.getElementById('password').value;
+	// 2. Gui value tu client den server
+
+	try {
+		const res = await axios.post('/api/auth/user/create', {
+			username,
+			email,
+			password,
+			role,
+		});
+		console.log('🚀 ~ file: register.js:14 ~ res', res);
+		if (res.status === 201) {
+			// window.location.href = '/client/login.html';
+		}
+	} catch (error) {
+		// log error
+	}
+};

a848331818c7e5edd37618ba1a121e1eba4f33e8
tanhoang0710 <tanhoang0710@gmail.com>
2022-12-08 10:01:20 +0700

    redux create store

--- redux-core-create/index.js
+++ redux-core-create/index.js
@@ -1,19 +1,41 @@
-// The Factory Pattern
+// store -> state duoc luu tru tren store
+// Khi chung ta muon thay doi state tren store thi cung ta thuc hien dispatch action
 
-const addCounter = () => {
-	let value = 0;
+const createStore = () => {
+	let state = 0;
 
-	const getValue = () => value;
+	const dispatch = (action) => {
+		//logic here -- thay doi state theo action
+		if (action.type === 'increment') state += action.payload;
+		if (action.type === 'decrement') state -= action.payload;
+	};
 
-	const addValue = (amount) => (value += amount);
+	const getState = () => {
+		return state;
+	};
 
 	return {
-		getValue,
-		addValue,
+		dispatch,
+		getState,
 	};
 };
 
-const counter = addCounter();
-console.log(counter.getValue());
-counter.addValue(5);
-console.log(counter.getValue());
+const store = createStore();
+// o trong component lay state tren store va render
+console.log('🚀 ~ file: index.js:21 ~ store', store.getState());
+
+// click increment
+store.dispatch({
+	type: 'increment',
+	payload: 3,
+});
+
+console.log(store.getState());
+
+// click increment
+store.dispatch({
+	type: 'decrement',
+	payload: 1,
+});
+
+console.log(store.getState());
